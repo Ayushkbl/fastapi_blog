34c4fbc95f3341e0c7ae2bb2fb0e83afa365ce13
ayushkbl <ayushverma7891@gmail.com>
2026-04-11 07:37:44 +0530

    Switched to PostgreSQL with Alembic

--- main.py
+++ main.py
@@ -23,9 +23,6 @@ from routers import users, posts
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
-    # Startup
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
     # Shutdown
     await engine.dispose
